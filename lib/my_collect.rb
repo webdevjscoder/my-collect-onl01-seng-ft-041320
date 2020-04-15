0f2a829582ce2a4cf02780fdccd4bf660f297f59
Juan Cobian <cobianjuan19@gmail.com>
2020-04-15 21:09:13 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,24 @@
+require 'pry'
 
+def my_collect(collection)
+  if block_given?
+    languages = []
+    i = 0
+    while i < collection.length
+      languages << yield(collection[i].upcase)
+      i += 1
+    end
+    
+    student_first_name = []
+     i = 0
+     while i < collection.length
+      student_first_name <<  yield(collection[i].split(' ').first)
+      i += 1
+    end
+  else
+      puts "Hey! No block was given!"
+  end
+    languages
+    student_first_name
+end 
 
